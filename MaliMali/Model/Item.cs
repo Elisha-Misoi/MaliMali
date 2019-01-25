d5ff5802b2cbf318c11008b3af3e4f463a84264d
Elisha Misoi <e.kmisoi@outlook.com>
2019-01-25 10:40:53 +0300

    [Finishes #163475638] added user model

---
+++ MaliMali/Model/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MaliMali.Model
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; }
+        public int Rating { get; set; }
+        
+
+        public Item()
+        {
+        }
+    }
+}
---
+++ MaliMali/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MaliMali.Model
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Uid { get; set; }
+        public string DP_Url { get; set; }
+        public string Phone_Number { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
